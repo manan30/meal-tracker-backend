fdc2cc80a920ea0788ba0b3150a074c5e70ef9c6
Manan Joshi <mananjoshi1995@gmail.com>
2020-02-07 22:59:19 -0500

    Adding API for generating recipe data

--- faker.js
+++ faker.js
@@ -33,11 +33,36 @@ const generateCategories = () => {
   return data;
 };
 
+const generateImages = () => {
+  const images = [];
+
+  for (let i = 0; i < 10; i += 1) {
+    const image = faker.image.food(155, 105);
+
+    images.push({ id: i, image });
+  }
+
+  return images;
+};
+
+const generateRecipeData = () => {
+  const images = new Array(10).fill(0).map(() => faker.image.food(150, 105));
+  const name = faker.commerce.product();
+  const ingredients = new Array(10).fill(0).map(faker.commerce.productMaterial);
+  const steps = new Array(8)
+    .fill(0)
+    .map(() => faker.lorem.sentences(Math.floor(Math.random() * (3 - 1)) + 1));
+
+  return { name, images, ingredients, steps };
+};
+
 module.exports = () => {
   const recipes = generateRecipes();
   const categories = generateCategories();
+  const images = generateImages();
+  const recipe = generateRecipeData();
 
-  const data = { recipes, categories };
+  const data = { recipes, categories, images, recipe };
 
   return data;
 };
