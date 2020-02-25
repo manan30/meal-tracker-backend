8f93011099f85fdb43cc739b7ad30172af5e7738
Manan Joshi <mananjoshi1995@gmail.com>
2020-02-25 18:33:18 -0500

    Adding new shell script for fake data

---
+++ scripts/pull-run-fake.sh
@@ -0,0 +1,7 @@
+cd $HOME/projects
+git clone git@github.com:manan30/meal-tracker-backend.git
+cd meal-tracker-backend
+git checkout --track origin/fake-data
+docker-compose up --detach
+cd ..
+rm -rfd meal-tracker-backend
