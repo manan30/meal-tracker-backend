0663f863162ed2d3919827393d641cf7c33fe33d
Manan Joshi <mananjoshi1995@gmail.com>
2020-02-25 18:28:16 -0500

    Changing workflows for different branches

--- scripts/pull-run.sh
+++ scripts/pull-run.sh
@@ -1,6 +1,10 @@
 cd $HOME/projects
 git clone git@github.com:manan30/meal-tracker-backend.git
 cd meal-tracker-backend
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+if [[ $# -gt 0]]; then
+  git checkout --track origin/fake-data
+fi
 docker-compose up --detach
 cd ..
 rm -rfd meal-tracker-backend
