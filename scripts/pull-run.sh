b207c5ef35c4c189441324337fbb89f2a0dbab8f
Manan Joshi <mananjoshi1995@gmail.com>
2020-02-25 17:55:16 -0500

    Running docker compose in detach mode

--- scripts/pull-run.sh
+++ scripts/pull-run.sh
@@ -1,6 +1,6 @@
 cd $HOME/projects
 git clone git@github.com:manan30/meal-tracker-backend.git
 cd meal-tracker-backend
-docker-compose up
+docker-compose up --detach
 cd ..
 rm -rfd meal-tracker-backend
