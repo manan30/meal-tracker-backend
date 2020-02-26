e756f6abc3fe1adc5655f8a91655b36b48d9cfca
Manan Joshi <mananjoshi1995@gmail.com>
2020-02-25 22:27:16 -0500

    Force building container on every push

--- scripts/pull-run-fake.sh
+++ scripts/pull-run-fake.sh
@@ -2,6 +2,6 @@ cd $HOME/projects
 git clone git@github.com:manan30/meal-tracker-backend.git
 cd meal-tracker-backend
 git checkout --track origin/fake-data
-docker-compose up --detach
+docker-compose up --detach --build app
 cd ..
 rm -rfd meal-tracker-backend
