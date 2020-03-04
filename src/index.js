5780987f6f5d068f713af02cfa17dc594ea05d7d
Manan Joshi <mananjoshi1995@gmail.com>
2020-03-04 18:43:50 -0500

    Removing all .js files

--- src/index.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const userRoutes = require('./routes/User')();
-
-const app = express();
-
-app.use('/user', userRoutes);
--- src/models/ShoppingList.js
+++
@@ -1,1 +0,0 @@
-const shoppingListSchema = new Schema({});
