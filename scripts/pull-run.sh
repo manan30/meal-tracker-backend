3662562ea74aa10eb28ac3de09dec2688cca7a85
Manan Joshi <mananjoshi1995@gmail.com>
2020-03-08 19:47:16 -0400

    Updating delpoy check

--- scripts/pull-run.sh
+++ scripts/pull-run.sh
@@ -3,4 +3,4 @@ git clone git@github.com:manan30/meal-tracker-backend.git
 cd meal-tracker-backend
 docker-compose --detach --env-file $HOME/.env up
 cd ..
-rm -rfd meal-tracker-backend
+# rm -rfd meal-tracker-backend
