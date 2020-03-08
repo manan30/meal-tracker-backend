8f4362b5df3fe3076a81e961bccd935a8a036989
Manan Joshi <mananjoshi1995@gmail.com>
2020-03-08 19:44:02 -0400

    Adding github workflow to run on pull_requests

--- scripts/pull-run.sh
+++ scripts/pull-run.sh
@@ -1,6 +1,6 @@
 cd $HOME/projects
 git clone git@github.com:manan30/meal-tracker-backend.git
 cd meal-tracker-backend
-docker-compose up --detach
+docker-compose --detach --env-file $HOME/.env up
 cd ..
 rm -rfd meal-tracker-backend
