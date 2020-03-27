2e103615b8d1c077f20e0372e624009ccaf23062
Manan Joshi <mananjoshi1995@gmail.com>
2020-03-27 18:40:40 -0400

    Solving errors on the middleware handlers

--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -1,14 +1,18 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import HttpException from '../utils/HttpException';
 
 export default function errorHandler(
   error: HttpException,
   req: Request,
-  res: Response
+  res: Response,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  _next: NextFunction
 ): void {
   const status = error.statusCode || 500;
   const message =
     error.message || 'Internal server error. Please try again in some time';
+
+  console.log({ status, message });
 
   res.status(status).send(message);
 }
--- src/middleware/notFound.middleware.ts
+++ src/middleware/notFound.middleware.ts
@@ -1,10 +1,12 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import HttpException from '../utils/HttpException';
 
 export default function notFoundHandler(
   error: HttpException,
   req: Request,
-  res: Response
+  res: Response,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  _next: NextFunction
 ): void {
   const message = 'Resource not found';
 
